d27228d805be57ae0f40c48438ed787c044a04e6
shiraz768 <hshiraz649@gmail.com>
2023-05-20 20:47:17 +0500

    Modified

--- module.js
+++ module.js
@@ -2,17 +2,18 @@ let num1 = document.getElementById("num1");
 let num2 = document.getElementById("num2");
 let  op = document.getElementById("op");
 let res = document.getElementById("res");
+let click = document.getElementById("click")
 
-export function result(){
+ function result(num1,num2,op,res){
     if(op.value == "+"){
-        return res.innerHTML = num1.value + num2.value;
+        return res.innerHTML = parseInt(num1.value) + parseInt(num2.value);
     }if(op.value == "-"){
-        return res.innerHTML = num1.value - num2.value;
-    }if(op.value == "*"){
-        return res.innerHTML = num1.value * num2.value;
-    }if(op.value == "/"){
-        return res.innerHTML = num1.value / num2.value;
+        return res.innerHTML = parseInt(num1.value) - parseInt(num2.value);
+    }if(op.value  == "*"){
+        return res.innerHTML = parseInt(num1.value) * parseInt(num2.value);
+    }if(op.value  == "/"){
+        return res.innerHTML = parseInt(num1.value) / parseInt(num2.value);
     }
 }
 
-export {num1,num2,op,res}
+export {num1,num2,op,res,click,result}
